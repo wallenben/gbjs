d568ac1e23955d32c37cee8b800e39ab10c63563
pbjpie <pbjpie@outlook.com>
2022-02-12 03:58:38 -0600

    static class implementation for all registers and clock

--- src/operands.ts
+++ src/operands.ts
@@ -1,6 +1,6 @@
 import { RegisterType } from "./cpu";
-import registers from "./registers";
-export function NOP(){
+import { registers, clock } from "./registers";
+export function NOP() {
     //nothing happens, 4 clock cycles
-    registers.clock.m += 4;
+    clock.m += 4;
 }
--- src/registers.ts
+++ src/registers.ts
@@ -4,36 +4,23 @@
    
    
    //8-bit registers
- class registers { 
-     a = 0; 
-     b: 0; 
-     c: 0; 
-     d: 0; 
-     e: 0;
-     h: 0; 
-     l: 0;
-     pc: 0; //program counter
-     sp: 0; //stack pointer 
-    clock: { m: 0, t: 0 };
-     static clock: any;
-    constructor(){
-        this.a = 0;
-        this.b = 0;
-        this.c = 0;
-        this.d = 0;
-        this.e = 0;
-        this.h = 0;
-        this.l = 0;
-        this.pc = 0
-        this.sp = 0
-        this.clock.m = 0;
-        this.clock.t = 0;
-    }
+ export class registers { 
+     static a = 0; 
+     static b = 0; 
+     static c = 0; 
+     static d = 0; 
+     static e = 0;
+     static h = 0; 
+     static l = 0;
+     static pc = 0; //program counter
+     static sp = 0; //stack pointer 
  }
+export class clock{
+    static m = 0;
+    static t = 0;
+}
     //16-bit registers
     
     //wtf is the m-clock?
     //t: clock cycles
     
-
-export default registers;
